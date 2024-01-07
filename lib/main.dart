2e7d621efaba2bfbc7be3f5a572b8ab8d9042138
Marlon Falcón Hernández <falconsoft.3d@gmail.com>
2024-01-07 13:31:54 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gamer_mvvm/src/presentation/pages/auth/login/login_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+
+      debugShowCheckedModeBanner: false,
+      
+      initialRoute: 'login',
+      
+      routes:{
+        'login': (context) => const LoginPage(),
+      },
+    );
+  }
+}
+
+
+
